15619c5d228bdcacc6b9c99159c15f4a9767fb73
Joshua Balao <joshuacbalao@gmail.com>
2020-07-01 21:26:16 -0400

    add object for scores

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,6 +1,7 @@
 var friends = [
         {
                 name: "Diana Prince",
-                photo: "https://i.insider.com/5ded5c4e79d757035d26ce62?width=1100&format=jpeg&auto=webp"
+                photo: "https://i.insider.com/5ded5c4e79d757035d26ce62?width=1100&format=jpeg&auto=webp",
+                scores: []
         }
 ]
