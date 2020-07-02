bb876187be5c6d5c4351f63025ad6285a5cdb07d
Joshua Balao <joshuacbalao@gmail.com>
2020-07-01 21:16:58 -0400

    add first friend

---
+++ app/data/friend.js
@@ -0,0 +1,6 @@
+var friends = [
+        {
+                name: "Diana Prince",
+                photo: "https://i.insider.com/5ded5c4e79d757035d26ce62?width=1100&format=jpeg&auto=webp"
+        }
+]
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* These are the Dependancies for the app; without these the app crashes*/
 var express = require("express")
 var bodyParser = require("body-parser")
 var path = require("path")
@@ -6,16 +7,17 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+/* This sets of the app so that it can handle the data parsing */ 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: `application/vnd.api.json`}))
 
-
+/*This is the Router for the app; it allows for information to be pulled*/
 require('.app/routing/apiRoutes.js')(app)
 require('.app/routing/htmlRoutes.js')(app)
 
-
+/*This is what starts the server and it will begin listening for commands*/
 app.listen(PORT, function() {
         console.log("App is listening on Port: " + PORT);
 });
