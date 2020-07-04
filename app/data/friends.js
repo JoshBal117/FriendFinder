72e4e0055a5bcfdc68ce6ce29e9f715833baccb8
Joshua Balao <joshuacbalao@gmail.com>
2020-07-04 11:57:19 -0400

    add updated friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
         {
                 name: "Kori Anders",
-                photo: "",
+                photo: "https://imgix-media.wbdndc.net/cms/filer_public_thumbnails/filer_public/58/60/5860419f-e126-4dc0-aa68-8af710bb8b36/starfire-profile-7b59c1-profile-talesofthenewteentitans4-1-v1-1500x2244-masthead.jpg__387x579_q85_crop_subsampling-2_upscale.jpg",
                 scores: [
                     1,
                     5,
@@ -17,23 +17,69 @@ var friendsArray = [
         },
         {
                 name: "Richard Grayson",
-                photo: "",
-                scores: []
+                photo: "https://vignette.wikia.nocookie.net/marvel_dc/images/c/cd/Nightwing_%28Earth-16%29.jpg/revision/latest/top-crop/width/360/height/450?cb=20130116112248",
+                scores: [
+                        1,
+                        1,
+                        5,
+                        5,
+                        4,
+                        2,
+                        8,
+                        3,
+                        4,
+                        2,
+                ]
         },
         {
                 name: "Victor Stone",
-                photo: "",
-                scores: []
+                photo: "https://i.pinimg.com/originals/41/9b/05/419b053c00924a7be9aab240c4df131a.jpg",
+                scores: [
+                        8,
+                        3,
+                        4,
+                        3,
+                        1,
+                        3,
+                        3,
+                        8,
+                        4,
+                ]
         },
         {
                 name: "Raven Roth",
-                photo: "",
-                scores: []
+                photo: "https://a1-images.myspacecdn.com/images02/119/6363d4490fa648a5bc015cb45d0107a7/full.jpg",
+                scores: [
+                        1,
+                        4,
+                        1,
+                        1,
+                        3,
+                        5,
+                        5,
+                        2,
+                        1,
+                        8,
+                        2,
+                ]
         },
         {
                 name: "Garfield Logan",
-                photo: "",
-               scores: []
+                photo: "https://vignette.wikia.nocookie.net/marvel_dc/images/0/03/Garfield_Logan_Earth-16_002.png/revision/latest?cb=20190711003222",
+               scores: [
+                       2,
+                       4,
+                       2,
+                       1,
+                       1,
+                       1,
+                       2,
+                       4,
+                       8,
+                       7,
+
+
+               ]
         },       
 ]
 
